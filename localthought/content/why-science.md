0b0f6d9a31f000f29277bd499ce0945bb9f11c27
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-03-26 20:41:09 +0000

    update

---
+++ localthought/content/why-science.md
@@ -0,0 +1,4 @@
+Title: Why I am a scientist
+Date: 2022-03-26 10:20
+Category: Local Thought
+
---
+++ localthought/pelicanconf.py
@@ -0,0 +1,32 @@
+AUTHOR = 'Toan Phung'
+SITENAME = 'Local Thought'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'GB'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = "theme"
+HEADER_COVER = "static/banner.png"
+COLOR_SCHEME_CSS = 'darkly.css'
+STATIC_PATHS = ["static"]
+# Blogroll
+# LINKS = (("1", "#"),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/noatgnuhp'),
+          ('linkedin', 'https://www.linkedin.com/in/toan-phung-347941a0/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
